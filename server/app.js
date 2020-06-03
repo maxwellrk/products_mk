60df3007939031f8fe487adea9df894bafe215b8
maxwellrk <maxkatz23@gmail.com>
2020-06-03 18:59:37 -0400

    Added cors options to express server

--- server/app.js
+++ server/app.js
@@ -1,20 +1,18 @@
 const express = require('express');
-
 const app = express();
 const cors = require('cors');
-
 const port = process.env.PORT || 43443;
-
-app.use(cors());
-
-app.options('*', cors());
-
+const path = require('path');
 const {
   getProductList,
   getIndividualProduct,
   getRelatedProducts,
   getProductStyles,
 } = require('./controllers');
+
+app.use(cors());
+
+app.options('*', cors());
 
 app.get('/products/list', getProductList);
 
@@ -27,3 +25,5 @@ app.get('/products/:product_id/styles', getProductStyles);
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+
+module.exports = app;
